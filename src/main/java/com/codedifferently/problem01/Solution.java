0ebe8571283bb2f54573f4b32c713d64f8131ff9
Khalil Brewington <kbrewing02@gmail.com>
2022-07-14 10:06:31 -0400

    updates

--- src/main/java/com/codedifferently/problem01/Solution.java
+++ src/main/java/com/codedifferently/problem01/Solution.java
@@ -1,5 +1,7 @@
 package com.codedifferently.problem01;
 
+import java.util.*;
+
 public class Solution {
     /**
      * You will be given an array of numbers, search the array and return the longest
@@ -10,7 +12,40 @@ public class Solution {
      * @param allNumbers
      * @return
      */
-    public String findLongestConsecutiveSet(Integer[] allNumbers){
-        return null;
+    public String findLongestConsecutiveSet(Integer[] allNumbers) {
+        int count = 0;
+        int largest = 0;
+        Set<Integer> consecutive = new HashSet<>();
+        Set<Integer> largestSet = new HashSet<>();
+        Map<Integer, Set<Integer>> hashmap = new HashMap<>();
+        Arrays.sort(allNumbers);
+
+        for (int i = 0; i < allNumbers.length; i++) {
+            if (i > 0 && allNumbers[i] == allNumbers[i - 1] + 1) {
+                count++;
+                consecutive.add(allNumbers[i - 1]);
+                consecutive.add(allNumbers[i]);
+            } else {
+                hashmap.put(consecutive.size(), consecutive);
+                consecutive = new HashSet<>();
+                count = 1;
+            }
+            largestSet = largestSet.size() >= consecutive.size() ? largestSet : consecutive;
+            largest = Math.max(largest, count);
+
+            if (i == allNumbers.length - 1) {
+                hashmap.put(largest, consecutive);
+            }
+        }
+        largestSet = hashmap.get(largest);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Integer num : largestSet) {
+            stringBuilder.append(num).append(" ");
+        }
+        String answer = stringBuilder.toString();
+        answer = answer.trim();
+        answer = answer + "}";
+        return answer;
     }
 }
--- src/main/java/com/codedifferently/problem02/Solution.java
+++ src/main/java/com/codedifferently/problem02/Solution.java
@@ -1,7 +1,19 @@
 package com.codedifferently.problem02;
 
+import java.util.HashMap;
+
 public class Solution {
-    public String howManyLettersDoYouSee(String input){
-        return null;
+    public String howManyLettersDoYouSee(String input) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        char[] strArray = input.toCharArray();
+
+        for (char ch : strArray) {
+            if(charCount.containsKey(ch)) {
+                charCount.put(ch, charCount.get(ch) + 1);
+            } else {
+                charCount.put(ch,1);
+            }
+        }
+        return charCount.get(input).toString();
     }
 }
